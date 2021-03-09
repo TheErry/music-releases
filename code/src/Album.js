279faaa1fe561c2e0eac5e1d8fd25cbd4eec5c2e
Linnéa Borealis <70851837+Polkapetunian@users.noreply.github.com>
2021-03-09 23:11:56 +0100

    started creating  Artist and Album component

---
+++ code/src/Album.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Artist from "./Artist";
+
+export const Album = Artist => {
+  return <h1>
+  <Artist name="Dua Lipa"/>
+</h1>
+}
--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import data from './data.json'
+import { Album } from "./Album"
 
 console.log(data)
 
+console.log(data.album)
+
 export const App = () => {
   return (
+    // <div>
+    //   <Album />
+    // </div>
     <div>
-      Find me in src/app.js!
+      <Album length="1"/>
     </div>
+  
   )
-}
+  };
