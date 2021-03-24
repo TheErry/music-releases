09b697cce4f51485b902bc6b28b718a0ab251031
Linnéa Borealis <70851837+Polkapetunian@users.noreply.github.com>
2021-03-24 06:53:56 +0100

    added hover effects

--- code/src/Components/AlbumImage.js
+++ code/src/Components/AlbumImage.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const AlbumImage = (props) => {
-    const image = props.image.url;
+  const image = props.image.url;
 
-    return (
-      <div className="album-image-container">
-        <img className="album-image" src={image} alt="Album"/>
-      </div>
-    )
-  }
+  return (
+    <div className="album-image-container">
+      <img className="album-image" src={image} alt="Album"/>
+    </div>
+  );
+};
   
   export default AlbumImage;
